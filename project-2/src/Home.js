2a134c5cacb9ed4f96d5cdf24f3ebe95d8796a1f
thummel1867 <thummel1867@gmail.com>
2022-07-20 18:10:04 -0400

    added home collage

--- project-2/src/Home.js
+++ project-2/src/Home.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to the emissions website!</h1>
-            <p>Find out about emissions in your region!</p>
-            <img className = "openingArt" src = "https://images.theconversation.com/files/354902/original/file-20200826-7069-1mp7gk0.jpg?ixlib=rb-1.1.0&rect=5%2C80%2C3589%2C1794&q=45&auto=format&w=1356&h=668&fit=crop" alt="home-img"/>
-        </div>
-    );
+  return (
+    <div className="home">
+      <h1>Welcome to the emissions website!</h1>
+      <p>Find out about emissions in your region!</p>
+      <div class="grid">
+        <div class="grid-item1"></div>
+        <div class="grid-item2"></div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5"></div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+        <div class="grid-item8"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
